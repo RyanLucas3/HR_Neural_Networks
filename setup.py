187057cb2a4276a1e5ffb1ddd7b0eca33891ebcc
Ryan Lucas <55145311+RyanLucas3@users.noreply.github.com>
2023-03-03 12:58:23 -0500

    setup

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ setup(
           'numpy',
           'cvxpy',
           'torch',
-           'time'
+          'warnings'
       ],
   classifiers=[
     'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
