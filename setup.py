ccbe63ce145d984a04b2065b99e1ecda16063521
Ryan Lucas <55145311+RyanLucas3@users.noreply.github.com>
2023-03-03 12:50:09 -0500

    setup

--- setup.py
+++ setup.py
@@ -3,13 +3,13 @@ from distutils.core import setup
 setup(
   name = 'HR_Neural_Networks',         # How you named your package folder (MyLib)
   packages = ['HR_Neural_Networks'],   # Chose the same as "name"
-  version = '0.0.0',      # Start with a small number and increase it with every change you make
+  version = '1.0.1',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'Holistic Robust Neural Networks',   # Give a short description about your library
   author = 'Amine Bennouna, Bart Van Parys, Ryan Lucas',                   # Type in your name
   author_email = 'ryanlu@mit.edu',      # Type in your E-Mail
   url = 'https://github.com/RyanLucas3/HR_Neural_Networks',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/RyanLucas3/HR_Neural_Networks/archive/refs/tags/0.0.0.tar.gz',
+  download_url = 'https://github.com/RyanLucas3/HR_Neural_Networks/archive/refs/tags/1.0.1.tar.gz',
   keywords = ['Neural Networks', 'Robustness', 'Machine Learning', "Data Science", "Adversarial Attacks"],   # Keywords that define your package best
   install_requires=[ # I get to this in a second
           'mosek',
